05356da2b9a198625d92c2ba39b3f08b7665aaf4
AndrewKov23 <andrewkov23@gmail.com>
2019-05-06 11:09:29 +0300

    Updates

---
+++ precache-manifest.745f9e9dec30eded024390daf909ae62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7338bf5091566342c3ba64716e48bef",
+    "url": "/TV-Shows/static/media/no-shows.e7338bf5.png"
+  },
+  {
+    "revision": "8953c5e078d6114525e4",
+    "url": "/TV-Shows/static/js/runtime~main.ac21190e.js"
+  },
+  {
+    "revision": "0f0e45f9987236898916",
+    "url": "/TV-Shows/static/js/main.3f40064c.chunk.js"
+  },
+  {
+    "revision": "77e610cf0523c41c985f",
+    "url": "/TV-Shows/static/js/2.5472c00a.chunk.js"
+  },
+  {
+    "revision": "0f0e45f9987236898916",
+    "url": "/TV-Shows/static/css/main.d0d0d91d.chunk.css"
+  },
+  {
+    "revision": "7f7357257c7752cb48415a1f26646115",
+    "url": "/TV-Shows/index.html"
+  }
+];
